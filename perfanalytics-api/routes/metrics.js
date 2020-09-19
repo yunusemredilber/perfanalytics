b522497a78649e4f100c826ab0a62459bbce9490
yunusemredilber <yunusemredilber@yandex.com>
2020-09-19 21:14:09 +0300

    Add simple metrics router

---
+++ perfanalytics-api/routes/metrics.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var Metric = require('../db/metric')
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  Metric.find().then((metrics) => {
+    res.json(metrics);
+  }).catch((err) => {
+    res.status(500);
+    res.json(err);
+  });
+});
+
+router.post("/", function(req, res, next){
+  console.log(req.body.fcp)
+  new Metric(req.body).save().then(metric => {
+    res.json({
+      message: 'Metric saved',
+      data: metric
+    });
+  }).catch((err) => {
+    res.status(500);
+    res.json({
+      message: 'Metric saving error',
+      error: err
+    });
+  });
+});
+
+module.exports = router;
