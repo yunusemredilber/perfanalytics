1bef0cd9a307f2ddfc2ff432b351c84d8be79930
yunusemredilber <yunusemredilber@yandex.com>
2020-09-19 17:01:56 +0300

    Change the structure of js client library

--- perfanalytics-js/src/index.js
+++ perfanalytics-js/src/index.js
@@ -1,42 +1,23 @@
-function Perfanalytics() {
-  this.init = () => {
-    if(!this.isPerformanceSupported()) {
-      return
+const initPerfanalytics = () => {
+  window.__perfanalytics = {
+    values: {
+      network_timings: {}
     }
-    this.startObserver()
-    console.log(window.performance)
-    
-    window.__perfanalytics = {
-      values: {
-        network_timings: {}
-      }
-    }
-
-    window.onload = this.send
   }
 
-  this.sendData = () => {
-    const { performance: { timing } } = window
-    const values = window.__perfanalytics.values
-
-    values['ttfb'] = asSeconds(timing.responseStart - timing.requestStart) // Time To First Byte
-    values['dom_load'] = asSeconds(timing.domContentLoadedEventEnd - timing.navigationStart)
-    values['window_load'] = asSeconds(timing.domInteractive - timing.navigationStart)
-    values['network_timings']['document'] = asSeconds(timing.responseEnd - timing.navigationStart)
-
-    console.log('***', values)
-    // TODO: Send the data without harming client performance
-  }
-
-  this.isPerformanceSupported = () => {
+  const isPerformanceSupported = () => {
     return window.performance && !!window.performance.getEntriesByType && !!window.performance.timing
   }
 
-  this.getPerfanalytics = () => window.__perfanalytics
+  if(!isPerformanceSupported()) {
+    return
+  }
 
-  this.asSeconds = ms => ms / 1000
+  const getPerfanalytics = () => window.__perfanalytics
+
+  const asSeconds = ms => ms / 1000
   
-  this.startObserver = () => {
+  const startObserver = () => {
     if(typeof(PerformanceObserver) === 'undefined') return
 
     const getTimingFromEntry = entry => ({
@@ -47,16 +28,16 @@ function Perfanalytics() {
   
     const observerEntryHandlers = {
       paint(entry) {
-        this.getPerfanalytics().values['fcp'] = this.asSeconds(entry.startTime)
+        getPerfanalytics().values['fcp'] = asSeconds(entry.startTime)
       },
       resource(entry) {
-        const network_timings = this.getPerfanalytics().values.network_timings
+        const network_timings = getPerfanalytics().values.network_timings
         if(network_timings[entry.initiatorType]) {
-          network_timings[entry.initiatorType].total += this.asSeconds(entry.responseEnd)
+          network_timings[entry.initiatorType].total += asSeconds(entry.responseEnd)
           network_timings[entry.initiatorType].items.push(getTimingFromEntry(entry))
         } else {
           network_timings[entry.initiatorType] = {
-            total: this.asSeconds(entry.responseEnd),
+            total: asSeconds(entry.responseEnd),
             items: [getTimingFromEntry(entry)]
           }
         }
@@ -70,6 +51,25 @@ function Perfanalytics() {
     })
     observer.observe({entryTypes: ['paint', 'resource']})
   }
+
+  startObserver()
+
+  const sendData = () => {
+    const { performance: { timing } } = window
+    const values = window.__perfanalytics.values
+
+    values['ttfb'] = asSeconds(timing.responseStart - timing.requestStart) // Time To First Byte
+    values['dom_load'] = asSeconds(timing.domContentLoadedEventEnd - timing.navigationStart)
+    values['window_load'] = asSeconds(timing.domInteractive - timing.navigationStart)
+    values['network_timings']['document'] = asSeconds(timing.responseEnd - timing.navigationStart)
+
+    console.log('***', values)
+    // TODO: Send the data without harming client performance
+  }
+
+  console.log(window.performance)
+
+  window.onload = sendData
 }
 
-Perfanalytics.init()
+initPerfanalytics()
