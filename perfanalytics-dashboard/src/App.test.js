ffd64bfdd9587771e4d68b595c44c0bd45fceb92
yunusemredilber <yunusemredilber@yandex.com>
2020-09-21 23:37:20 +0300

    Add a simple test and test script

--- perfanalytics-dashboard/src/App.test.js
+++ perfanalytics-dashboard/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders ttfb chart title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/TTFB/i);
   expect(linkElement).toBeInTheDocument();
 });
