4b87d168f3948c7ff96138bac79a61bd8d166705
yunusemredilber <yunusemredilber@yandex.com>
2020-09-22 20:53:52 +0300

    Send metrics data only when all fields are appropriate

--- perfanalytics-js/src/index.js
+++ perfanalytics-js/src/index.js
@@ -1,10 +1,24 @@
 const initPerfanalytics = (serviceUrl = 'https://perfanalytics-app.herokuapp.com') => {
+  let isMetricsSent = false
+
   window.__perfanalytics = {
     values: {
       files: [],
       url: window.location.href,
-      user_agent: navigator.userAgent
+      user_agent: navigator.userAgent,
+      fcp: null,
+      ttfb: null,
+      dom_load: null,
+      window_load: null,
+      navigation_started_at: null,
     }
+  }
+
+  const hasEmptyField = (object) => {
+    for (let key in object) {
+      if(object[key] === null) return true
+    }
+    return false
   }
 
   const isPerformanceSupported = () => {
@@ -30,7 +44,11 @@ const initPerfanalytics = (serviceUrl = 'https://perfanalytics-app.herokuapp.com
   
     const observerEntryHandlers = {
       paint(entry) {
+        if(entry.name !== 'first-contentful-paint') return
+
         getPerfanalytics().values['fcp'] = asSeconds(entry.startTime)
+        // Sometimes fcp info comes after window load event.
+        sendDataIfAppropriate()
       },
       resource(entry) {
         const files = getPerfanalytics().values.files
@@ -43,23 +61,21 @@ const initPerfanalytics = (serviceUrl = 'https://perfanalytics-app.herokuapp.com
         observerEntryHandlers[entry.entryType](entry)
       }
     })
-    observer.observe({entryTypes: ['paint', 'resource']})
+    observer.observe({ entryTypes: ['paint', 'resource'], buffered: true })
   }
 
   startObserver()
 
-  const sendData = async () => {
-    const { performance: { timing } } = window
-    const values = window.__perfanalytics.values
+  const sendDataIfAppropriate = async () => {
+    const values = getPerfanalytics().values
 
-    values['ttfb'] = asSeconds(timing.responseStart - timing.requestStart) // Time To First Byte
-    values['dom_load'] = asSeconds(timing.domContentLoadedEventEnd - timing.navigationStart)
-    values['window_load'] = asSeconds(timing.domInteractive - timing.navigationStart)
-    values['navigation_started_at'] = new Date(timing.navigationStart).toString()
-    values['files']['document'] = asSeconds(timing.responseEnd - timing.navigationStart)
+    if(hasEmptyField(values) || isMetricsSent) {
+      console.log('values has empty field', values)
+      return
+    }
 
     console.log('***', values)
-
+    isMetricsSent = true
     const rawResponse = await fetch(`${serviceUrl}/metrics`, {
       method: 'POST',
       headers: {
@@ -67,12 +83,25 @@ const initPerfanalytics = (serviceUrl = 'https://perfanalytics-app.herokuapp.com
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(values)
-    });
-    const res = await rawResponse.json();
-    console.log(res);
+    })
+    const res = await rawResponse.json()
+    console.log(res)
+  }
+
+  const onWindowLoad = () => {
+    const { performance: { timing } } = window
+    const values = window.__perfanalytics.values
+
+    values.ttfb = asSeconds(timing.responseStart - timing.requestStart) // Time To First Byte
+    values.dom_load = asSeconds(timing.domContentLoadedEventEnd - timing.navigationStart)
+    values.window_load = asSeconds(new Date().valueOf() - timing.navigationStart)
+    values.navigation_started_at = new Date(timing.navigationStart).toString()
+    values.files['document'] = asSeconds(timing.responseEnd - timing.navigationStart)
+
+    sendDataIfAppropriate()
   }
 
   console.log(window.performance)
 
-  window.onload = sendData
+  window.onload = onWindowLoad
 }
